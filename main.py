b699de408f099c4013763cd3aa7659a340fd49cc
Ruslan <xsiruse@gmail.com>
2019-12-10 01:16:19 +0300

    0.0.1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime, date
-
+from pprint import pprint
 import vk_api
 
 TOKEN = '8adc8c028adc8c028adc8c02548ab1847688adc8adc8c02d7547fedba187a9a4a0bbbd9'
@@ -9,35 +9,16 @@ fields = ['about', 'activities', 'bdate', 'blacklisted', 'blacklisted_by_me', 'b
           'relation', 'schools', 'sex', 'timezone', 'tv', 'universities']
 fi = ','.join(fields)
 
-def vk(login, password):
-    vk_session = vk_api.VkApi(login, password)
-    vk_session.auth()
-    return vk_session.get_api()
 
+def slices(slice, iters):
+    a = 1
+    b = slice
+    lol = []
+    for n in range(1, iters):
+        lol.append(list(range(a, b)))
+        a += slice
+        b += slice
+    return lol
 
-
-
-
-class User:
-
-    def __init__(self, id):
-        interests = opse.users.get(user_ids=id, fields='%s' % fi)[0]
-        self.b_date = datetime.strptime(interests['bdate'], '%d.%m.%Y')
-        self.age = date.today().year - self.b_date.year - ((date.today().month, date.today().day) <
-                                                           (self.b_date.month, self.b_date.day))
-        self.gender = interests['sex']
-        self.relation = interests['relation']
-        self.groups = opse.groups.get()
-        self.friends = opse.friends.get()
-        self.interests = {}
-        self.country = interests['country']
-        self.city = interests['city']
-
-
-if __name__ == "__main__":
-    login = input('Введите номер телефона или логин: ')
-    password = input('Введите пароль: ')
-    opse = vk(login, password)
-
-    me = User(opse.users.get()[0]['id'])
-
+if __name__ == '__main__':
+    pass
